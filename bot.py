37cc3063971685779dcf4188b6b2e32e0508912e
Wojciech Kwolek <me@irth.pl>
2021-05-19 22:50:09 +0200

    some comments

--- bot.py
+++ bot.py
@@ -7,40 +7,70 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, PicklePersiste
 
 class Bot:
     def __init__(self, token, admin, device):
+        # Admin contains the ID of the user that can allow other users to print
         self.admin = admin
+        # Device is the CUPS printer name from `lpstat -e`
         self.device = device
+
+        # Using a pickle in ./ to persist the allowlist
         self.persistence = PicklePersistence(filename='data.pickle')
+
+        # Setting up python-telegram-bot
         self.updater = Updater(
             token=token, use_context=True, persistence=self.persistence)
+
+        # Registering command handlers
         self.dispatcher = self.updater.dispatcher
         self.dispatcher.add_handler(CommandHandler('start', self.cmd_start))
         self.dispatcher.add_handler(CommandHandler('add', self.cmd_add))
         self.dispatcher.add_handler(CommandHandler('deny', self.cmd_deny))
 
+        # This message handler will only trigger on PDF files
         self.dispatcher.add_handler(MessageHandler(
             Filters.document.pdf, self.on_message))
 
     def ensure_bot_data(self, context):
+        """
+        A helper function to ensure that the schema of bot_data is correct.
+        """
         if "allowed" not in context.bot_data:
             context.bot_data["allowed"] = {}
 
     def is_admin(self, user, context):
+        """
+        Returns true if the user is an admin.
+        """
         return user.id == self.admin
 
     def is_allowed(self, user, context):
+        """
+        Returns true if the user is allowed to print.
+        """
         self.ensure_bot_data(context)
         return self.is_admin(user, context) or (user.id in context.bot_data["allowed"])
 
     def allow(self, user_id, context):
+        """
+        Adds the user_id to the allowlist.
+        """
         self.ensure_bot_data(context)
         context.bot_data["allowed"][user_id] = True
 
     def deny(self, user_id, context):
+        """
+        Removes the user_id from the allowlist if the user is in it.
+        """
         self.ensure_bot_data(context)
         context.bot_data["allowed"].pop(user_id, None)
 
     def cmd_start(self, update, context):
+        """
+        /start - the first command that the user runs. Telegram gives you a
+        button to run it instead of a text input when you first interact with
+        the bot.
+        """
         user = update.effective_user
+
         message = "Hello. "
         if self.is_allowed(user, context):
             message += "You can print by sending a PDF file to this bot."
@@ -48,6 +78,7 @@ class Bot:
                 message += " You are an administrator. Add people to the bot using /add <numerical_ID>."
         else:
             message += f"You are not allowed to print. Ask the admin to add you. Your ID is: {user.id}."
+
         context.bot.send_message(
             chat_id=update.effective_chat.id, text=message)
 
